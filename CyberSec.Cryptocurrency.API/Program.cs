2750fd2e5a46a1f1ccbb6ee3714dcf6a859ab286
Ardo <ardotrtrian@gmail.com>
2022-04-01 01:55:11 +0400

    commented out HSTS uage in non development environment

--- CyberSec.Cryptocurrency.API/Program.cs
+++ CyberSec.Cryptocurrency.API/Program.cs
@@ -28,12 +28,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
 }
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//else
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
 
